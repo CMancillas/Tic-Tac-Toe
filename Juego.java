01074e21ff0c21b50b489e01829e76790d140334
CMancillas <carlosmancillas4799@gmail.com>
2025-03-07 17:29:35 -0700

    Creacion de tablero inicial en la clase Juego y tablero con movimientos

--- Juego.java
+++ Juego.java
@@ -3,7 +3,7 @@ public class Juego
     private String[][] tablero;
     private boolean jugadorActual;
 
-
+    
     public Juego()
     {
         this( true );
@@ -11,35 +11,77 @@ public class Juego
     
     public Juego( boolean jugador)
     {
-        tablero = new String[ 3 ][ 5 ];
-        
-        for ( int fila = 0; fila < 3; fila++ )
-        {
-            for ( int columna = 0; columna < 5; columna ++)
-            {
-                if ( columna % 2 != 0 )
-                    tablero[ fila ][ columna ] = "|";
-                else
-                    tablero[ fila ][ columna ] = " ";
-            }
-        }
-
+        tablero = new String[ 3 ][ 3 ];
+        for (int fila = 0; fila < tablero.length; fila++)
+            for ( int columna = 0; columna < tablero[1].length; columna++ )
+                tablero[fila][columna] = "-";
+            
         jugadorActual = jugador;
     } // Fin del constructor
 
+
+    public void mostrarTableroInicial()
+    {
+        int posicion_tablero = 1;
+
+        for ( int fila = 0; fila < 6; fila++)
+        {
+            for ( int columna = 0; columna < tablero.length; columna++)
+            {
+                if ( fila % 2 == 0)
+                {
+                    if ( columna != 0 )
+                    System.out.print("|" + " ");
+                
+                    System.out.print(posicion_tablero + " ");
+                    posicion_tablero++;
+                }     
+            }
+            
+            if ( fila == 1 || fila == 3 )
+            {
+                for( int i = 0; i < 10; i++)
+                {
+                    System.out.print("-");
+                }    
+            }               
+            System.out.print("\n");
+        }
+    }
     public void mostrarTablero()
     {
-        for ( int fila = 0; fila < 3; fila++)
-        {
-            System.out.println();
-            for ( int columna = 0; columna < 11; columna++ )
-            {
-                System.out.print("-");
+        
+        
 
-                if ( columna / 4 == 0 )
-                    System.out.print( "|" );
+        for ( int fila = 0; fila < 6; fila++)
+        {
+            for ( int columna = 0; columna < tablero.length; columna++)
+            {
+                if ( fila % 2 == 0)
+                {
+                    if ( columna != 0 )
+                    System.out.print("|" + " ");
+                    
+                    
+                    System.out.print(tablero[(fila / 2)][columna] + " ");
+                    
+                }     
             }
-            System.out.println();
+            
+            if ( fila == 1 || fila == 3 )
+            {
+                for( int i = 0; i < 10; i++)
+                {
+                    System.out.print("-");
+                }    
+            }               
+            System.out.print("\n");
         }
-    } // fin del metodo mostrarTablero
+    } // fin del metodo mostrarTableroInicial
+
+    public void rellenarTablero(int[] posicion, String jugador)
+    {
+        if (tablero[posicion[0]][posicion[1]].equals("-") )  
+            tablero[posicion[0]][posicion[1]] = jugador;
+    }
 }
--- PruebaJuego.java
+++ PruebaJuego.java
@@ -3,7 +3,14 @@ public class PruebaJuego
     public static void main( String[] args )
     {
         Juego nuevoJuego = new Juego();
+        JugadorHumano polly = new JugadorHumano("Polly", 'x');
 
+        
+        nuevoJuego.rellenarTablero(polly.obtenerCasilla(1), "x");
+        nuevoJuego.rellenarTablero(polly.obtenerCasilla(8), "x");
+        nuevoJuego.rellenarTablero(polly.obtenerCasilla(5), "o");
+        nuevoJuego.rellenarTablero(polly.obtenerCasilla(3), "o");
+        nuevoJuego.mostrarTableroInicial();
         nuevoJuego.mostrarTablero();
     }
     
